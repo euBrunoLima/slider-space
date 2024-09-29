3ac86784a08cd29a2b39e9793491bb61445162d7
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2024-09-29 01:29:54 -0300

    primeiro commit

---
+++ arquivo-js/script.js
@@ -0,0 +1,50 @@
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let preview = document.querySelectorAll('.preview .item');
+
+let countItem = items.length;
+let itemActive = 0;
+
+next.onclick = function() {
+    itemActive = itemActive + 1;
+    if (itemActive >= countItem) {
+        itemActive = 0;
+    }
+
+    showSlider();
+};
+
+prev.onclick = function() {
+    itemActive = itemActive - 1;
+    if (itemActive < 0) {
+        itemActive = countItem - 1;
+    }
+    showSlider();
+};
+
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 5000);
+
+function showSlider() {
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let previewActiveOld = document.querySelector('.preview .item.active');
+    itemActiveOld.classList.remove('active');
+    previewActiveOld.classList.remove('active');
+
+    items[itemActive].classList.add('active');
+    preview[itemActive].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 3000);
+}
+
+preview.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        itemActive = index;
+        showSlider();
+    });
+});
